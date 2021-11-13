17199e22f599e3c75a35af209a423cc9ac1f0589
texao <alex.cheikh.sar@gmail/com>
2021-11-13 13:27:10 +0100

    ajout des fichiers

---
+++ tp4mutation/src/main/java/tp4mutationid/TableauPleinException.java
@@ -0,0 +1,5 @@
+package tp4mutationid;
+
+public class TableauPleinException extends Exception {
+
+}
---
+++ tp4mutation/src/main/java/tp4mutationid/TableauVideException.java
@@ -0,0 +1,5 @@
+package tp4mutationid;
+
+public class TableauVideException extends Exception {
+
+}
